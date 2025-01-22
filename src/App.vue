e5362e73addfde5329a1b2ab8412b84f7844dd86
Sheldon <Sheldon.Wei@example.com>
2025-01-22 15:49:44 +0800

    合并逻辑到vue组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
-<template>
-  <RubiksCube />
-</template>
-
 <script lang="ts" setup>
 import RubiksCube from './components/RubiksCube.vue'
 </script>
 
+<template>
+  <RubiksCube />
+</template>
+
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #f0f0f0;
-}
 </style>
